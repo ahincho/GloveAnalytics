994f429deb33ecea2064c5e845b81eaafa711146
ahincho <ahincho@unsa.edu.pe>
2023-01-19 16:32:13 -0500

    Operaciones en la Base de Datos

---
+++ GloveQueries/MySQLConnection.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace GloveDatabase
+{
+    class MySQLiteConnection
+    {
+        
+        private string Source = "D:/ProyectosVisualStudio/GloveAnalytics/GloveQueries/GloveTest.db";
+        private SQLiteConnection? Instance = null;
+
+        public SQLiteConnection GetConnection()
+        {
+            if (Instance == null)
+            {
+                Instance = new SQLiteConnection("Data Source = " + Source);
+                Console.WriteLine("Data Source = " + Source);
+            }
+            Console.WriteLine("Abierto");
+            return Instance;
+        }
+
+        public void CloseConnection()
+        {
+            if (Instance != null)
+            {
+                Instance.Close();
+                Instance = null;
+            }
+            Console.WriteLine("Cerrado");
+        }
+
+    }
+}
---
+++ GloveQueries/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace GloveDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Class SQLite Connection");
+            MySQLiteConnection m = new MySQLiteConnection();
+            SQLiteConnection s = m.GetConnection();
+            m.CloseConnection();
+        }
+
+    }
+}
