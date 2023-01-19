974af1ed7a84212066b6c628da93a07c9a09340b
ahincho <ahincho@unsa.edu.pe>
2023-01-19 12:06:56 -0500

    Agregar archivos de proyecto.

---
+++ GloveForms/MainScreen.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace GloveForms
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(218)))), ((int)(((byte)(104)))));
+            this.BackgroundImage = global::GloveForms.Properties.Resources.GloveBackground;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "MainScreen";
+            this.Text = "CITESOFT - GloveApp";
+            this.Load += new System.EventHandler(this.MainScreen_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
