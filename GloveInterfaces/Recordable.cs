22cd5e92011a32adea96bebd09eb56d9e2b36835
ahincho <ahincho@unsa.edu.pe>
2023-01-20 10:36:37 -0500

    Interfaz Grabable - Recordable

---
+++ GloveInterfaces/Recordable.cs
@@ -0,0 +1,22 @@
+﻿
+namespace GloveInterfaces
+{
+    
+    public interface Recordable
+    {
+
+        /*
+         * An Recordable Object is that object which has these two
+         * methods, the first method returns table's name which has
+         * the records. Then, the second method returns the fields
+         * of the table (concatenated) and the last method returns
+         * all the data which are part of the table
+         */
+
+        public string getTableName();
+        public string getFieldsWithCommas();
+        public string mergedWithCommas();
+
+    }
+
+}
