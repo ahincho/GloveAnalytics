a894630be6f9cb7caed754ae21c44a600704629a
ahincho <ahincho@unsa.edu.pe>
2023-01-20 00:07:33 -0500

    Pruebas para la Conexion y Entidades

--- GloveQueries/Program.cs
+++ GloveQueries/Program.cs
@@ -5,17 +5,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
+using GloveClasses;
 
 namespace GloveDatabase
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+
+            // This is a pretty and quick testing of the SQLConnection
+            // If you wanna try, just delete the comment annotation
+
+            /*
             Console.WriteLine("Class SQLite Connection");
             MySQLiteConnection m = new MySQLiteConnection();
             SQLiteConnection s = m.GetConnection();
             m.CloseConnection();
+            */
+
+            // Here we are testing the created Entities to manage the
+            // objects or records that we need to save in the Database
+
+            /*
+            HandAngles h = new HandAngles(0.25f, 0.25f, 0.25f, 0.25f, 0.25f);
+            Console.WriteLine(h.mergedWithCommas());
+            Session a = new Session(1, 1, "19/01/2023", 10.5f, 50.0f, 100, 3, h, 1);
+            Console.WriteLine(a.mergedWithCommas());
+            */
+
         }
 
     }
