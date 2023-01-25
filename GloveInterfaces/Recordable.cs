f0b2aeec066daf18f136aef4af8a817e41f34bcc
ahincho <ahincho@unsa.edu.pe>
2023-01-25 12:00:47 -0500

    Obtener Query para Insertar Objetos

--- GloveInterfaces/Recordable.cs
+++ GloveInterfaces/Recordable.cs
@@ -5,17 +5,38 @@ namespace GloveInterfaces
     public interface Recordable
     {
 
-        /*
-         * An Recordable Object is that object which has these two
-         * methods, the first method returns table's name which has
-         * the records. Then, the second method returns the fields
-         * of the table (concatenated) and the last method returns
-         * all the data which are part of the table
-         */
+        // Method to recover the name of its table
 
         public string getTableName();
+        
+        // Method to get the concatenated field name of its table
+
         public string getFieldsWithCommas();
+        
+        // Method to get the object data concatenated with commas
+
         public string mergedWithCommas();
+
+        // Method to get the SQL Query to insert the object in its table
+
+        public string getInsertQuery()
+        {
+            return "INSERT INTO " + getTableName() + "(" + getFieldsWithCommas() + ") VALUES(" + mergedWithCommas() + ");"
+        }
+
+        // Method to get the Status Field name in its table
+
+        public string getStatusFieldName()
+        {
+            return getTableName().Substring(0, 3) + "Estado";
+        }
+
+        // Method to get the Id Field name in its table
+
+        public string getIdFieldName()
+        {
+            return getTableName().Substring(0, 3) + "Id";
+        }
 
     }
 
