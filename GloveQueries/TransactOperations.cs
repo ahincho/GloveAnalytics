7ad07756138b198029f76a705e432f1abeb7e7c2
ahincho <ahincho@unsa.edu.pe>
2023-03-31 11:44:26 -0500

    Operaciones Transaccionales y Queries

--- GloveQueries/TransactOperations.cs
+++ GloveQueries/TransactOperations.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Data.Entity.Infrastructure;
 using System.Data.SQLite;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using GloveClasses;
@@ -22,14 +23,145 @@ namespace GloveQueries
         private static SQLiteCommand mCommand;
         private static SQLiteDataReader mDataReader;
 
-        public static void RecoverSesions(int patientId, IRecordable newRecord )
+        public static List<int> RecoverSesions(int patientId)
         {
-            string query = "INSERT INTO " + newRecord.GetTableName() + "(" + newRecord.GetFieldsWithCommas() + ") VALUES(" + newRecord.MergedWithCommas() + ");";
+            string query = "SELECT SesId FROM Session WHERE SesPatientId = " + patientId + " ORDER BY SesId DESC";
+            List<int> sessionsId = new List<int>();
             sCon.Open();
             mCommand = new SQLiteCommand(query, sCon);
-            mCommand.ExecuteNonQuery();
-            sCon.Close();
+            try
+            {
+                mDataReader = mCommand.ExecuteReader();
+                while (mDataReader.Read())
+                {
+                    sessionsId.Add(Convert.ToInt32(mDataReader[0]));
+                }
+                mDataReader.Close();
+                return sessionsId;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                sCon.Close();
+            }
+            return sessionsId;
         }
+
+        public static double RecoverMetersOfSession(int sesId)
+        {
+            string query = "SELECT SesMeters FROM Session WHERE SesId = " + sesId;
+            double meters = -1;
+            sCon.Open();
+            mCommand = new SQLiteCommand(query, sCon);
+            try
+            {
+                mDataReader = mCommand.ExecuteReader();
+                while (mDataReader.Read())
+                {
+                    meters = Convert.ToDouble(mDataReader[0]);
+                }
+                mDataReader.Close();
+                return meters;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                sCon.Close();
+            }
+            return meters;
+        }
+
+        public static double RecoverAverageMeters(int patientId)
+        {
+            string query = "SELECT AVG(SesMeters) FROM Session WHERE SesPatientId = " + patientId;
+            double metersAvg = -1;
+            sCon.Open();
+            mCommand = new SQLiteCommand(query, sCon);
+            try
+            {
+                mDataReader = mCommand.ExecuteReader();
+                while (mDataReader.Read())
+                {
+                    metersAvg = Math.Round(Convert.ToDouble(mDataReader[0]));
+                }
+                mDataReader.Close();
+                return metersAvg;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                sCon.Close();
+            }
+            return metersAvg;
+        }
+
+        public static double RecoverCoinsOfSession(int sesId)
+        {
+            string query = "SELECT SesCoins FROM Session WHERE SesId = " + sesId;
+            double coins = -1;
+            sCon.Open();
+            mCommand = new SQLiteCommand(query, sCon);
+            try
+            {
+                mDataReader = mCommand.ExecuteReader();
+                while (mDataReader.Read())
+                {
+                    coins = Convert.ToDouble(mDataReader[0]);
+                }
+                mDataReader.Close();
+                return coins;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                sCon.Close();
+            }
+            return coins;
+        }
+
+        public static double RecoverAverageCoins(int patientId)
+        {
+            string query = "SELECT AVG(SesCoins) FROM Session WHERE SesPatientId = " + patientId;
+            double coinsAvg = -1;
+            sCon.Open();
+            mCommand = new SQLiteCommand(query, sCon);
+            try
+            {
+                mDataReader = mCommand.ExecuteReader();
+                while (mDataReader.Read())
+                {
+                    coinsAvg = Math.Round(Convert.ToDouble(mDataReader[0]));
+                }
+                mDataReader.Close();
+                return coinsAvg;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                sCon.Close();
+            }
+            return coinsAvg;
+        }
+
+    }
+
+    public static double RecoverAnglesOfSession(int patientId, int sessionId)
+    {
 
     }
 
